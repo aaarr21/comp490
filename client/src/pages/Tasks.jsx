592c6496ddbc8996a8a7d80a49f80ca3d69b41c2
SebRousSunga <sebastian.sunga.834@.my.csun.edu>
2025-02-26 16:56:49 -0800

    added date picker component, styling is off but functions, added select options functionallity

--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -4,6 +4,10 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faCircleXmark, faXmark,faPaperclip, faFaceSmile,faCalendar, faUserPlus, faA } from '@fortawesome/free-solid-svg-icons';
 import '../components/styles/Tasks.css';
 import Picker from 'emoji-picker-react'; // for the emoji section
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import Select from 'react-select';
 import { text } from '@fortawesome/fontawesome-svg-core';
 
 const Tasks = () => {
@@ -13,21 +17,21 @@ const Tasks = () => {
     const [shownewTask,setnewTask] = useState(false) // state to determine if the new task should be viewable, 
                                                      // this gets modified by clicking upon the new task button.
 
-                                                     useEffect(()=>{  
-                                                        const logginInCheck = async () =>{
-                                                        const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/WorkBoard`, {
-                                                            credentials: 'include',
-                                                        })    
-                                                        if(response.status === 401)
-                                                            console.log("AAAAAA");
-                                                        else{
-                                                            setLoggedin(true);
-                                                        }
+           useEffect(()=>{  
+                 const logginInCheck = async () =>{
+                const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/Task`, {
+                            credentials: 'include',
+                        })    
+                             if(response.status === 401)
+                                 console.log("AAAAAA");
+                                     else{
+                                          setLoggedin(true);
+                                                }
                                                        // const data = await response.json();
                                                         //console.log(data);
-                                                    }
-                                                       logginInCheck();
-                                                      } ,[]);
+                                            }
+                                             logginInCheck();
+                 } ,[]);
    
       const createNewTask = () => {               
           setnewTask(!shownewTask);
@@ -67,23 +71,57 @@ const NewTask = ({invertTask, taskStatus}) => {
         invertTask(!taskStatus);
      }
 
-     
+     /* 
+        Note: The list below exist for me to first implement the select options and see if I can grab and use the options 
+        for the Task object tbd. The actual iteartion will include an api call to the mysql database to create 
+        an array for the departments(probably in some table for departments), and then a created array
+         for the people within that department, another api call.
+     */
+
+     const DepartmentList = [ //Test list for the select option
+        {value: "CompSci", label: "CompSci"},
+        {value: "Biology", label: "Biology"},
+        {value: "Archives", label: "Archives"},
+        {value: "Administration", label: "Administration"}
+     ];
+
+      const PeopleList = [ //Test List for the people option
+        {value: "Sebastian Sunga", label:"Sebastian"},
+        {value: "John Dong" , label:"John"},
+        {value: "Bianca Loera", label:"Bianca"},
+        {value: "Alyssa Gomez", label: "Alyssa"},
+        {value:"Bryan Abrego", label:"Bryan"}
+      ]
+
+
 
      const [files,setFiles] = useState([]); //Handle the files
 
+     const [departList,setdepartList] = useState(DepartmentList); //this feels wasteful for now, keep for when we can really do api calls to database.
+
+     const [person,setPerson] = useState(null);
+
      const[displayfile,setDisplayFile] = useState(false); //Display the attachments when they exist in context.
 
-     const[displayEmoji,setEmoji] = useState(false);
+     const[displayEmoji,setEmoji] = useState(false); // for emoji picker render
 
-     const textRef = useRef(null);
+     const[displayDate,setDisplayDate] = useState(false); // to render date picker
+     
+     const[taskDate,settaskDate] = useState(null);
 
+     const textRef = useRef(null); //Ref hook for text area
+  
+
+     const [selectedDepartment,setselectedDepartment] = useState();
+        //Handle file upload from user by concating to current file array.
      const handleFileChange = (event) => {
         if(event.target.files){ //Ensure that user did upload a pdf.          
             try {
             
                     setFiles(Array.from(event.target.files));
                     setDisplayFile(true);                   
-                    const yoinkedFiles = Array.from(event.target.files); //use from to convert from FilesList to Array. I need to do this so I can use the map array function.
+                    const yoinkedFiles = Array.from(event.target.files); //use from to convert from FilesList to Array. 
+                                                                         // I need to do this so I can use the map array function.
                   
                    let combinedList = [...files,...yoinkedFiles]; //Use spread operator to create a final list of all file elements
                   
@@ -98,14 +136,30 @@ const NewTask = ({invertTask, taskStatus}) => {
      const renderEmoji = (event) =>{
        
          event.preventDefault();
-         textRef.current.value += "HUH";
+         
          setEmoji(!displayEmoji);
+     }
+      //starter to render date picker
+     const renderDate = (event) =>{
+         event.preventDefault();
+         setDisplayDate(!displayDate);
      }
 
      const appendEmoji = (emojiObject) =>{
         
         textRef.current.value += emojiObject.emoji;
         setEmoji(!displayEmoji);
+     }
+
+     const appendDate = (date) =>{
+        settaskDate(date);
+        
+        setDisplayDate(false);
+     }
+
+     const handleDepartmentSelection = (chosenDepartment) =>{
+        setselectedDepartment(chosenDepartment);
+        
      }
 
      const deleteAttachment = (fileId) => {
@@ -120,8 +174,16 @@ const NewTask = ({invertTask, taskStatus}) => {
         
         <div className="new-task-container">   
            
-            <div className="new-task-head-close"><h2 className="task-head">Create New Task</h2> <button className="close-button" onClick={closeTask}><FontAwesomeIcon icon={faXmark} className="close-icon" /></button></div>
-            <div className="new-task-people"><label for="person" className="task-label"> For</label> <p className="person" id="person"> Bianca</p>   <label for="placeholderTwo" className="task-label"> In</label> <p className="person" id="placeholderTwo">Project</p> </div>
+            <div className="new-task-head-close"><h2 className="task-head">Create New Task</h2> 
+            <button className="close-button" onClick={closeTask}><FontAwesomeIcon icon={faXmark} className="close-icon" /></button>
+            </div>
+
+            <div className="new-task-people"><label for="person" className="task-label"> For</label> 
+            <DepartmentSelect options={PeopleList} onChanges={ (person) => setPerson(person)}/>  
+            
+            <label for="placeholderTwo" className="task-label"> In</label> 
+               <DepartmentSelect options={DepartmentList} onChanges={handleDepartmentSelection}/>
+             </div>
             <form className="new-task-form">            
                 <textarea placeholder="Description...." id="textArea" ref={textRef}></textarea>
                   
@@ -131,11 +193,16 @@ const NewTask = ({invertTask, taskStatus}) => {
                   <button type="button"  onClick={renderEmoji} id="emoji-picker" style = {{display: 'none'}}>   </button>         
                     <div className="emoji-position">   { displayEmoji && <Picker onEmojiClick={appendEmoji} />  }  </div>
                    <label htmlFor="emoji-picker"> <FontAwesomeIcon icon={faFaceSmile}  className="auxillery-icon" /> </label>
-                   <input type="file" style = {{display: 'none'}} onChange={handleFileChange} id="attachment-upload" accept=".pdf,.xml,.docx" multiple />
+                   <input type="file" style = {{display: 'none'}} onChange={handleFileChange} id="attachment-upload"
+                    accept=".pdf,.xml,.docx" multiple />
                    <label htmlFor="attachment-upload">
                     <FontAwesomeIcon icon={faPaperclip} className="auxillery-icon"/>
                     </label>
-                   <FontAwesomeIcon icon={faCalendar}  className="auxillery-icon"/>
+
+                    <button type="button"  onClick={renderDate} id="date-picker" style = {{display: 'none'}}>   </button>
+                   <label htmlFor="date-picker">   <FontAwesomeIcon icon={faCalendar}  className="auxillery-icon"/> </label>
+                   <div className="date-position" >   { displayDate && <DatePicker 
+                        selected ={taskDate} onChange={appendDate} /> }  </div>
                    <FontAwesomeIcon icon={faUserPlus} className = "person-share" />
                 </div>
                 <div>
@@ -156,13 +223,29 @@ const NewTask = ({invertTask, taskStatus}) => {
 };
 
 
+const DepartmentSelect = ({options,onChanges}) => {
+
+       const cpyList = options;
+  
+    
+
+    return (
+        <Select
+            options={cpyList}
+            onChange={onChanges} />
+        
+    )
+}
+
+
 
 const FileInfo = ({id,fileName, deleteAttachment}) => {
       let fileID = id;
    
     return (
         <div className="file-card">
-            {fileName} <button  onClick={()=> {deleteAttachment(fileID)}} type="button"><FontAwesomeIcon icon={faXmark} className="close"/> </button>
+            {fileName} <button  onClick={()=> {deleteAttachment(fileID)}} type="button"><FontAwesomeIcon icon={faXmark} 
+            className="close"/> </button>
         </div>
     )
 }
